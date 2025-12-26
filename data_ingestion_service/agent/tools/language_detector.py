de9fb04e48d1c0ab7d2db31a665cb57f4008a4be
Asen Dulakshana Kavinda <s22010362@ousl.lk>
2025-12-26 18:23:36 +0530

    update lang decetor to translate

--- data_ingestion_service/agent/tools/language_detector.py
+++ data_ingestion_service/agent/tools/language_detector.py
@@ -1,21 +1,33 @@
-
+from deep_translator import GoogleTranslator
 from langchain.tools import tool
 import pycld2 as cld2
+import nltk
 
 from data_ingestion_service.utils.logger_config import log
 from data_ingestion_service.utils.exception_config import ProjectException
 
-@tool
-def detect_language(text: str):
+try:
+    nltk.download('punkt', quiet=True)
+    nltk.download('punkt_tab', quiet=True)
+except Exception as e:
+    log.error(f'--------------------------------{str(e)}')
+
+
+
+
+# @tool
+def detect_language_translate(text: str):
     """Detects languages in the provided text, which may contain segments in multiple languages.
     Returns a structured dictionary where keys are text segments and values include the language name and code."""
-    extract_sentences = {}
 
+    # if there is no text to translate return {}
     if not text:
         log.warning("No text provided for `detect_language` tool...")
         return {}
 
     try:
+        extract_sentences = {}
+
         is_reliable, text_bytes_found, details, vectors = cld2.detect(text, returnVectors=True)
 
         # Correct extraction using byte slicing
@@ -24,12 +36,21 @@ def detect_language(text: str):
             segment_bytes = text_bytes[offset:offset + length]
             segment = segment_bytes.decode('utf-8')
 
+            if not segment:
+                log.info("No segment found for `detect_language` tool...")
+                continue
+            supported_langs = ['en', 'fr', 'de']
+            sentences = nltk.sent_tokenize(segment, language=lang_code.lower() if lang_code.lower() in supported_langs else 'english')
+
+            translator = GoogleTranslator(source='auto', target='en')
+            translated_sentences = translator.translate_batch(sentences)
+
             extract_sentences[segment] = {
-                "language": lang_name,
-                "language_code": lang_code,
+                "original_sentences": sentences,
+                "translated_sentences": translated_sentences,
             }
 
-            log.info(f"Segment: {segment[:10]}, detect language{lang_name}")
+            log.info(f"Segment: {segment[:10]}, detect language{lang_name}, translated sentence: {translated_sentences[:10]}")
 
         return extract_sentences
     except Exception as e:
@@ -42,9 +63,9 @@ def detect_language(text: str):
 
 if __name__ == '__main__':
     # uncomment if only need to test the `detect_language` function
-    # text = "Bonjour le monde! Ceci est une phrase en français. හල වර්ගය. நான் உன்னை காதலி்கிறேன்"
-    # extract_sentences = detect_language(text)
-    # print(extract_sentences)
+    text = "Bonjour le monde! Ceci est une phrase en français. හල වර්ගය. நான் உன்னை காதலி்கிறேன்"
+    extract_sentences = detect_language_translate(text)
+    print(extract_sentences)
 
 
 
